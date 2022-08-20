b0d13949c8a432ce777fa00f1d010d81d9a1bf74
Atinuke Ayangade <aayangade@gmail.com>
2022-08-20 03:33:03 -0400

    added functional prework files to repository

---
+++ Prework/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  Prework
+//
+//  Created by Atinuke Ayangade on 8/18/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tipAmountLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var billAmountTextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func calculateTip(_ sender: Any) {
+        //get bill amount from text field input
+        let bill = Double(billAmountTextField.text!) ?? 0
+        
+        //get total tip by multiplying tip * tipPercentage
+        let tipPercentages = [0.12, 0.18, 0.2]
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        //update tip amount label
+        tipAmountLabel.text = String(format: "$%.2f", tip)
+        
+        //update total amount
+        totalLabel.text = String (format: "$%.2f", total)
+    }
+}
+
